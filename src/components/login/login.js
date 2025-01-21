876a23e6f16b80eacb6b6ec3e5ff02f5bc5e72c1
pedro.moraes <pedro.moraes@amlconsulting.com.br>
2025-01-21 20:01:44 -0300

    css da tela de login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -27,58 +27,65 @@ const Login = ({}) => {
     }
 
     return <div className="telaLogin">
-        <img className="bg" src="/assets/imgs/portrait-woman-holding-card 1.png"/>
-        <form onSubmit={handleSubmit} className="loginForm">
+        <div className="bg-wrapper">
+            <img className="bg" src="/assets/imgs/portrait-woman-holding-card 1.png"/>
+            <img class="logo" src="/assets/imgs/logobranco.png" alt="Logotipo" />
+
+        </div>
         
-            <div className="loginBloco1">
-                <h1>Bem vindo <span>:)</span></h1>   
-                <h2>Entre na sua conta</h2>
-            </div>
+        <div className="blocoForm">
+            <form onSubmit={handleSubmit} className="loginForm">
+            
+                <div className="loginBloco1">
+                    <h1>Bem vindo <span>:)</span></h1>   
+                    <h2>Entre na sua conta</h2>
+                </div>
 
-            <div className="loginBloco2">
-                <label> Usuário
-                    <input
-                        type="text"
-                        id="User"
-                        name="name"
-                        value={userName}
-                        onChange={(e) => setUserName(e.target.value)}/>
-                </label>
-                <label>Senha
-                    <input
-                        type="password"
-                        id="password"
-                        name="senha" 
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}/>
-                </label>
-                
-                <div className="suporteLogin">
-                    <div className="suporteLoginLembrar">
+                <div className="loginBloco2">
+                    <label> Usuário
                         <input
-                            type="checkbox"
-                            id="lembrar"
-                            name="lembrar"
-                            value={lembrar}
-                            onChange={(e) => setLembrar(e.target.value)}/>
-                        <label for="lembrar"> Lembrar dispositivo </label>
-                    </div>
-
-
-                    <a href="#">Esqueceu sua senha?</a>
+                            type="text"
+                            id="User"
+                            name="name"
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}/>
+                    </label>
+                    <label>Senha
+                        <input
+                            type="password"
+                            id="password"
+                            name="senha" 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>
+                    </label>
                     
+                    <div className="suporteLogin">
+                        <div className="suporteLoginLembrar">
+                            <input
+                                type="checkbox"
+                                id="lembrar"
+                                name="lembrar"
+                                value={lembrar}
+                                onChange={(e) => setLembrar(e.target.value)}/>
+                            <label for="lembrar"> Lembrar dispositivo </label>
+                        </div>
 
+
+                        <a href="#">Esqueceu sua senha?</a>
+                        
+
+                    </div>
+                    {msgLogin}
                 </div>
-                {msgLogin}
-            </div>
-            <div className="loginBloco3">
-                <div>
+                <div className="loginBloco3">
                     <button className="access">Entrar</button>
+                    <h2>Não tem conta? <span>Faça uma agora</span></h2>
                 </div>
-                <h2>Não tem conta? Faça uma agora</h2>
-            </div>
 
-        </form>
+            </form>
+        </div>
+
+
 
 
     </div>
